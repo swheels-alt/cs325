8329d38b016e0f9c059e5fcd1f437b3270770ba6
Sam Wheeler <samwheeler@Mac.lan>
2025-06-04 18:27:39 -0500

    merged the file

--- prog1.py
+++ prog1.py
@@ -1,3 +1,3 @@
-print("hello world from branch feature1")
+print("hello world from branch siue 325")
 print("This is a new line in the feature1 branch")
 print("This is a new line in the main branch")
